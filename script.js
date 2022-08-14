8b11030d6388779dd3dabe5c764e1f020e6c3e91
eddev <eddevjo@gmail.com>
2022-08-14 22:53:48 +0100

    Finished basics rock paper scissors / no ui yet

--- script.js
+++ script.js
@@ -1,49 +1,68 @@
+const btns = document.querySelectorAll(".hand");
+const results = document.querySelector(".result");
+const buttonPlayAgain = document.querySelector(".reset");
+
 let choices = ["rock", "paper", "scissor"];
+let playerLives = 0;
+let computerLives = 0;
 
 let getComputerChoice = () => {
   return choices[Math.floor(Math.random() * choices.length)];
 };
 
-let checkPlayerSelection = () => {
-  let playerSelection = window.prompt("Enter your hand: ").toLowerCase();
-  if (
-    playerSelection !== choices[0] &&
-    playerSelection !== choices[1] &&
-    playerSelection !== choices[2]
-  ) {
-    alert("Bad input! try again.");
-    return checkPlayerSelection();
-  } else {
-    return playerSelection;
-  }
-};
+function endGame(playerLives, computerLives) {
+  if (playerLives == 5 || computerLives == 5) {
+    btns.forEach((btn) => {
+      btn.setAttribute("disabled", "");
 
-let playRound = () => {
-  const playerSelected = checkPlayerSelection();
-  console.log("player choice" + playerSelected);
+      if (playerLives > computerLives) {
+        results.innerText = `Congrats you won`;
+      } else {
+        results.innerText = `baaad you lost`;
+      }
+    });
+  }
+}
 
-  const computerSelection = getComputerChoice();
-  console.log("computer choice" + computerSelection);
+function resetGame() {
+  buttonPlayAgain.setAttribute("enabled", "");
+  buttonPlayAgain.addEventListener("click", () => {
+    console.log("reset");
+    window.location.reload();
+  });
+}
 
-  if (playerSelected === computerSelection) {
-    return 0;
-  }
-  if (playerSelected == "rock" && computerSelection == "scissor") {
-    return 1;
-  }
-  if (playerSelected == "rock" && computerSelection == "paper") {
-    return -1;
-  }
-  if (playerSelected == "paper" && computerSelection == "scissor") {
-    return -1;
-  }
-  if (playerSelected == "paper" && computerSelection == "rock") {
-    return 1;
-  }
-  if (playerSelected == "scissor" && computerSelection == "paper") {
-    return 1;
-  }
-  if (playerSelected == "scissor" && computerSelection == "rock") {
-    return -1;
-  }
-};
+function playRound() {
+  let playerSelected;
+  btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      playerSelected = btn.className;
+      console.log("player choice " + playerSelected);
+      const computerSelection = getComputerChoice();
+      console.log("computer choice " + computerSelection);
+      switch (true) {
+        case playerSelected === computerSelection:
+          results.innerText = `it's a draw`;
+          console.log("draw");
+          break;
+        case playerSelected === "rock" && computerSelection === "scissor":
+        case playerSelected === "paper" && computerSelection === "rock":
+        case playerSelected === "scissor" && computerSelection === "paper":
+          playerLives += 1;
+          results.innerText = `You win`;
+          console.log("win");
+          break;
+
+        default:
+          results.innerText = `You loose`;
+          computerLives += 1;
+
+          break;
+      }
+      endGame(playerLives, computerLives);
+      resetGame();
+    });
+  });
+}
+
+playRound();
